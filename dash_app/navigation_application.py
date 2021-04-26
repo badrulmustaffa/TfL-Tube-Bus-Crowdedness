c009d9826e978224346cc9b112027591557224b0
badru <muhammad.mustaffa.18@ucl.ac.uk>
2021-04-26 04:46:18 +0100

    Badrul

--- dash_app/navigation_application.py
+++ dash_app/navigation_application.py
@@ -6,16 +6,15 @@ import dash_core_components as dcc
 from dash.dependencies import Output, Input, State
 
 from dash_app.layout_template import navbar
-
-## Creating a Dash app
 from dash_app.navigation_functions import AreaList, CreateBorders
 
+FA = "https://use.fontawesome.com/releases/v5.12.1/css/all.css"
 
 def init_dashboard(flask_app):
     """Create a Plotly Dash dashboard"""
     dash_app = dash.Dash(server=flask_app,
                          routes_pathname_prefix="/navigation_dash/",
-                         external_stylesheets=[dbc.themes.BOOTSTRAP])
+                         external_stylesheets=[dbc.themes.BOOTSTRAP, FA])
 
     ## Creating the app layout
     dash_app.layout = html.Div(children=[
@@ -31,35 +30,26 @@ def init_dashboard(flask_app):
                             fluid=True, children=[
                                 dbc.FormGroup([
                                     html.Br(),
-                                    html.Label(['Transportation mode:',
-                                                dcc.Dropdown(id='mean_select',
-                                                             options=[{"label": x, "value": x} for x in
-                                                                      ['Bus', 'Tube']],
-                                                             value='Bus')],
-                                               style={'width': '100%', 'display': 'inline-block'}
-                                               ),
 
-                                    # html.Label(['Transportation mode:',
-                                    #             dbc.Button("BUS", id="bus_select", color="success", outline=False),
-                                    #             dbc.Button("TUBE", id="tube_select", color="success", outline=True),
-                                    #             dcc.Dropdown(id='mean_select',
-                                    #                          options=[{"label": x, "value": x} for x in
-                                    #                                   ['Bus', 'Tube']],
-                                    #                          value='Bus')],
-                                    #            style={'width': '100%', 'display': 'inline-block'}
-                                    #            ),
+                                    dbc.Toast(
+                                        [dbc.Button([html.I(className="fas fa-bus mr-2"), "BUS"]
+                                                    , id="bus_select", color="success", outline=False,
+                                                    active=True, style={'margin-right': 10}),
+                                         dbc.Button([html.I(className="fas fa-subway mr-2"), "TRAIN"]
+                                                    , id="tube_select", color="success", outline=True)],
+                                        header="Transportation mode:",
+                                        ),
 
-                                    html.Br(),
-                                    html.Label([dbc.Badge("Start area:", pill=True, color="primary"),
-                                                dcc.Dropdown(id='start_select', placeholder='Search area', value=None)],
-                                               style={'width': '100%', 'display': 'inline-block'}),
+                                    dbc.Toast(
+                                        [dcc.Dropdown(id='start_select', placeholder='Search area', value=None)],
+                                        header="Start area:",
+                                        icon="primary",),
 
-                                    html.Br(),
-                                    html.Label([dbc.Badge("Destination:", pill=True, color="danger"),
-                                                dcc.Dropdown(id='end_select', placeholder='Search area', value=None)],
-                                               style={'width': '100%', 'display': 'inline-block'}),
+                                    dbc.Toast(
+                                        [dcc.Dropdown(id='end_select', placeholder='Search area', value=None)],
+                                        header="Destination:",
+                                        icon="danger",),
 
-                                    html.Br(),
                                     html.Div(children=[
                                         dbc.Button("Clear", id="clear_button", color="primary", className="mr-2"),
                                         dbc.Button("Go", id="go_button", color="primary", className="mr-2")
@@ -90,60 +80,59 @@ def init_dashboard(flask_app):
 ## Create callback for changing line from dropdown
 def init_callback(app):
     @app.callback([Output("start_select", "options"),
-                   Output("end_select", "options")],
-                  [Input("mean_select", "value")])
-    def update_area_dropdown(mean_select):
-        drop = [{"label": x, "value": x} for x in AreaList(mean_select)['Name']]
-        return drop, drop
+                   Output("end_select", "options"),
+                   Output("bus_select", "n_clicks"),
+                   Output("tube_select", "n_clicks"),
+                   Output("bus_select", "outline"),
+                   Output("tube_select", "outline"),
+                   Output("bus_select", "active"),
+                   Output("tube_select", "active"),
+                   ],
+                  [Input("bus_select", "n_clicks"),
+                   Input("tube_select", "n_clicks"),
+                   ])
+    def mean_selection(bus_n_clicks, tube_n_clicks):
+        mean_select = 'Bus'
+        bus_clicked = False
+        tube_clicked = True
+        bus_bold = True
+        tube_bold = False
 
-    @app.callback([Output("line_figure", "children")],
-                  [Input("mean_select", "value"),
+        if bus_n_clicks is not None:
+            mean_select = 'Bus'
+            bus_clicked = False
+            tube_clicked = True
+            bus_bold = True
+            tube_bold = False
+
+        if tube_n_clicks is not None:
+            mean_select = 'Tube'
+            bus_clicked = True
+            tube_clicked = False
+            bus_bold = False
+            tube_bold = True
+
+        drop = [{"label": x, "value": x} for x in AreaList(mean_select)['Name']]
+
+        return drop, drop, None, None, bus_clicked, tube_clicked, bus_bold, tube_bold
+
+    @app.callback(Output("line_figure", "children"),
+                  [Input("bus_select", "outline"),
+                   Input("tube_select", "outline"),
                    Input("start_select", "value"),
                    Input("end_select", "value")])
-    def render_figure(mean_select, start_select, end_select):
+    def render_figure(bus_clicked, tube_clicked, start_select, end_select):
+        mean_select = 'Bus'
+        if not bus_clicked:
+            mean_select = 'Bus'
+        if not tube_clicked:
+            mean_select = 'Tube'
         fig = CreateBorders(mean_select, start_select, end_select)
         return dcc.Graph(figure=fig)
 
-    # @app.callback([Output("start_select", "options"),
-    #                Output("end_select", "options"),
-    #                Output("bus_select", "n_clicks"),
-    #                Output("tube_select", "n_clicks"),
-    #                Output("bus_select", "outline"),
-    #                Output("tube_select", "outline"),
-    #                ],
-    #               [Input("bus_select", "n_clicks"),
-    #                Input("tube_select", "n_clicks"),
-    #                Input("bus_select", "outline"),
-    #                Input("tube_select", "outline")
-    #                ])
-    # def mean_selection(bus_n_click, tube_n_click, bus_clicked, tube_clicked):
-    #     mean_select = 'Bus'
-    #     if bus_n_click is not None:
-    #         mean_select = 'Bus'
-    #         bus_clicked = False
-    #         tube_clicked = True
-    #         bus_n_click = None
-    #
-    #     elif tube_n_click is not None:
-    #         mean_select = 'Tube'
-    #         tube_clicked = False
-    #         bus_clicked = True
-    #         tube_n_click = None
-    #
-    #     drop = [{"label": x, "value": x} for x in AreaList(mean_select)['Name']]
-    #     return drop, drop, bus_n_click, tube_n_click, bus_clicked, tube_clicked
-    #
-    # @app.callback([Output("line_figure", "children")],
-    #               [  # Input("mean_select", "value"),
-    #                   Input("bus_select", "outline"),
-    #                   Input("tube_select", "outline"),
-    #                   Input("start_select", "value"),
-    #                   Input("end_select", "value")])
-    # def render_figure(bus_clicked, tube_clicked, start_select, end_select):
-    #     mean_select = 'Bus'
-    #     if not bus_clicked:
-    #         mean_select = 'Bus'
-    #     if not tube_clicked:
-    #         mean_select = 'Tube'
-    #     fig = CreateBorders(mean_select, start_select, end_select)
-    #     return dcc.Graph(figure=fig)
+    @app.callback([Output("start_select", "value"),
+                   Output("end_select", "value")],
+                  [Input("clear_button", "n_clicks")])
+    def clear_selection(clear_n_clicks):
+        if clear_n_clicks is not None:
+            return None, None

8b9b63af1c1d0e9bcdbb9b009f059b8f3397ef2d
badru <muhammad.mustaffa.18@ucl.ac.uk>
2021-04-30 05:36:07 +0100

    Badrul

--- dash_app/layout_template.py
+++ dash_app/layout_template.py
@@ -1,5 +1,6 @@
 import base64
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 import dash_html_components as html
 
 
@@ -56,3 +57,73 @@ def nav_buttons():
                              ]),
                                        ])
     return full_navbar
+
+
+def navigation_template():
+    page = dbc.Container(fluid=True, children=[
+        # First row
+        dbc.Row([
+            # First column
+            dbc.Col(width=4, children=[
+                html.H2('TfL Zone 1 Navigation'),
+                html.P(id="indicator"),
+                html.P("Fill in your journey information below to check the route crowdedness"),
+                dbc.Card(children=[
+                    dbc.Container(
+                        fluid=True, children=[
+                            dbc.FormGroup([
+                                html.Br(),
+                                dbc.Toast(
+                                    [dbc.Button(
+                                        [html.I(className="fas fa-bus mr-2"),
+                                         "BUS"]
+                                        , id="bus_select", color="success",
+                                        outline=False, n_clicks=0,
+                                        active=True, style={'margin-right': 10}),
+                                        dbc.Button(
+                                            [html.I(className="fas fa-subway mr-2"),
+                                             "TRAIN"]
+                                            , id="tube_select", color="success",
+                                            outline=True, n_clicks=0)],
+                                    header="Transportation mode:",
+                                ),
+
+                                dbc.Toast(
+                                    [dcc.Dropdown(id='start_select',
+                                                  placeholder='Search area',
+                                                  value=None)],
+                                    header="Start area:",
+                                    icon="primary", ),
+
+                                dbc.Toast(
+                                    [dcc.Dropdown(id='end_select',
+                                                  placeholder='Search area',
+                                                  value=None)],
+                                    header="Destination:",
+                                    icon="danger", ),
+
+                                html.Div(children=[
+                                    dbc.Button("Clear", id="clear_button", type='reset',
+                                               color="primary", className="mr-2",
+                                               n_clicks=0),
+                                    dbc.Button("Go", id="go_button",
+                                               color="primary", className="mr-2",
+                                               n_clicks=0,
+                                               external_link=True)
+                                ])
+                            ])
+                        ])
+                ])
+            ]),
+
+            # Second column
+            dbc.Col(width=8, children=[
+                dbc.Card(children=[
+                    dbc.Container(fluid=True, style={'display': 'inline-block', 'width': '100%'},
+                                  id="line_figure")
+                ]),
+            ]),
+        ])
+
+    ])
+    return page
--- dash_app/navigation_application.py
+++ dash_app/navigation_application.py
@@ -3,7 +3,7 @@ import dash
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
-from dash_app.layout_template import logo, nav_buttons
+from dash_app.layout_template import logo, nav_buttons, navigation_template
 from dash_app.navigation_callback import init_callback
 
 FA = "https://use.fontawesome.com/releases/v5.12.1/css/all.css"
@@ -18,73 +18,7 @@ def init_dashboard(flask_app):
     ## Creating the app layout
     dash_app.layout = html.Div(id='page_content', children=[
         html.Header(nav_buttons()),
-
-        dbc.Container(fluid=True, children=[
-            # First row
-            dbc.Row([
-                # First column
-                dbc.Col(width=4, children=[
-                    html.H2('TfL Zone 1 Navigation'),
-                    html.P(id="indicator"),
-                    html.P("Fill in your journey information below to check the route crowdedness"),
-                    dbc.Card(children=[
-                        dbc.Container(
-                            fluid=True, children=[
-                                dbc.FormGroup([
-                                    html.Br(),
-                                    dbc.Toast(
-                                        [dbc.Button(
-                                            [html.I(className="fas fa-bus mr-2"),
-                                             "BUS"]
-                                            , id="bus_select", color="success",
-                                            outline=False, n_clicks=0,
-                                            active=True, style={'margin-right': 10}),
-                                            dbc.Button(
-                                                [html.I(className="fas fa-subway mr-2"),
-                                                 "TRAIN"]
-                                                , id="tube_select", color="success",
-                                                outline=True, n_clicks=0)],
-                                        header="Transportation mode:",
-                                    ),
-
-                                    dbc.Toast(
-                                        [dcc.Dropdown(id='start_select',
-                                                      placeholder='Search area',
-                                                      value=None)],
-                                        header="Start area:",
-                                        icon="primary", ),
-
-                                    dbc.Toast(
-                                        [dcc.Dropdown(id='end_select',
-                                                      placeholder='Search area',
-                                                      value=None)],
-                                        header="Destination:",
-                                        icon="danger", ),
-
-                                    html.Div(children=[
-                                        dbc.Button("Clear", id="clear_button", type='reset',
-                                                   color="primary", className="mr-2",
-                                                   n_clicks=0),
-                                        dbc.Button("Go", id="go_button",
-                                                   color="primary", className="mr-2",
-                                                   n_clicks=0,
-                                                   external_link=True)
-                                    ])
-                                ])
-                            ])
-                    ])
-                ]),
-
-                # Second column
-                dbc.Col(width=8, children=[
-                    dbc.Card(children=[
-                        dbc.Container(fluid=True, style={'display': 'inline-block', 'width': '100%'},
-                                      id="line_figure")
-                    ]),
-                ]),
-            ])
-
-        ])
+        html.Main(id="web_page", children=[navigation_template()]),
 
     ])
 

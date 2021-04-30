8c93bf2bd209d890f08dc8c3dfa0a6a377c69f8a
farhan mirza <zcemfmb@ucl.ac.uk>
2021-04-30 16:54:46 +0100

    added scratch.ipynb in the dash_app

--- my_app/Analysis/functions.py
+++ my_app/Analysis/functions.py
@@ -3,14 +3,21 @@ import pandas as pd
 
 # data = pd.read_excel('data/multi-year-station-entry-and-exit-figures-dataset-with-zone1.xlsx',
 #                      sheet_name='2017 Entry & Exit (Zone 1)', skiprows=6)
-from my_app.main.functions import dataprocessor
-mean = 'Bus'
-start = 'Bayswater, Notting Hill Gate'
-end = 'Tower Hill'
+data = pd.read_excel('../data/multi-year-station-entry-and-exit-figures.xls',
+                     sheet_name='2017 Entry & Exit (Zone 1)', skiprows=6)
 
-df, a, b = dataprocessor(mean, start, end)
+df = pd.DataFrame(data)
+df1 = pd.DataFrame(index=range(23), columns=['total']).fillna(0)
+df2 = df['Group Alphabet'].drop_duplicates().dropna().reset_index()
+
+for x, poo in df1.iterrows():
+    for y, lines in df.iterrows():
+        if x == lines['Group Number']:
+            poo['total'] += lines['million']
+
+df3 = pd.concat([df1, df2['Group Alphabet']], axis=1, join='inner')
 
 # wide_df = px.data.medals_wide()
 
-fig = px.bar(df, x="Group Alphabet", y="total", title="Wide-Form Input")
+fig = px.bar(df3, x="Group Alphabet", y="total", title="Wide-Form Input")
 fig.show()
